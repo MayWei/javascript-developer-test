4c142a02610f38c0c38ac3c04f5807203233656b
MayWei <weiminlcb@gmail.com>
2025-07-30 08:35:33 +1000

    implement the solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,17 @@
-const { httpGet } = require('./mock-http-interface');
-
+const { httpGet } = require("./mock-http-interface");
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  const result = await Promise.all(
+    urls.map(async (url) => {
+      const data = await httpGet(url);
+      const body = JSON.parse(data.body);
+      return data.status === 200
+        ? { "Arnie Quote": body.message }
+        : { FAILURE: body.message };
+    })
+  );
+  console.log(result);
+  return result;
 };
 
 module.exports = {
